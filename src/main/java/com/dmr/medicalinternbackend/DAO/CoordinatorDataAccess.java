3bc1bba96713b57d01482d6ddade03d18e6de359
demiriabdurrahman@gmail.com <Demir1234-+>
2022-07-03 17:13:20 +0300

    Revert "Backend"
    This reverts commit 69a34c396314c91356d6cb5000867ba3e0844477.
    
    # Conflicts:
    #	pom.xml
    #	src/main/java/com/dmr/medicalinternbackend/Controller/CoordinatorsController.java
    #	src/main/java/com/dmr/medicalinternbackend/Controller/InstitutesController.java
    #	src/main/java/com/dmr/medicalinternbackend/Controller/StudentsController.java
    #	src/main/java/com/dmr/medicalinternbackend/DAO/AttendingDataAccess.java
    #	src/main/java/com/dmr/medicalinternbackend/DAO/CourseDataAccess.java
    #	src/main/java/com/dmr/medicalinternbackend/DAO/SpecialityDataAccess.java
    #	src/main/java/com/dmr/medicalinternbackend/DAO/StudentDataAccess.java
    #	src/main/java/com/dmr/medicalinternbackend/Entities/AttendingPhysician.java
    #	src/main/java/com/dmr/medicalinternbackend/Entities/Coordinator.java
    #	src/main/java/com/dmr/medicalinternbackend/Entities/Course.java
    #	src/main/java/com/dmr/medicalinternbackend/Entities/PatientLogForm.java
    #	src/main/java/com/dmr/medicalinternbackend/Entities/Speciality.java
    #	src/main/java/com/dmr/medicalinternbackend/Entities/Student.java
    #	src/main/java/com/dmr/medicalinternbackend/Exception/ResourceNotFoundException.java
    #	src/main/java/com/dmr/medicalinternbackend/MedicalInternBackendApplication.java
    #	src/main/java/com/dmr/medicalinternbackend/Service/coordinator/CoordinatorService.java
    #	src/main/java/com/dmr/medicalinternbackend/Service/coordinator/ICoordinatorService.java
    #	src/main/java/com/dmr/medicalinternbackend/Service/course/CourseService.java
    #	src/main/java/com/dmr/medicalinternbackend/Service/course/ICourseService.java
    #	src/main/java/com/dmr/medicalinternbackend/Service/speciality/ISpecialityService.java
    #	src/main/java/com/dmr/medicalinternbackend/Service/speciality/SpecialityService.java
    #	src/main/java/com/dmr/medicalinternbackend/Service/student/StudentService.java
    #	src/main/resources/application.properties

--- src/main/java/com/dmr/medicalinternbackend/DAO/CoordinatorDataAccess.java
+++
@@ -1,7 +0,0 @@
-package com.dmr.medicalinternbackend.DAO;
-
-import com.dmr.medicalinternbackend.Entities.Coordinator;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CoordinatorDataAccess extends JpaRepository<Coordinator, Integer> {
-}
--- src/main/java/com/dmr/medicalinternbackend/DAO/InstituteDataAccess.java
+++
@@ -1,7 +0,0 @@
-package com.dmr.medicalinternbackend.DAO;
-
-import com.dmr.medicalinternbackend.Entities.Institute;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InstituteDataAccess extends JpaRepository<Institute,Integer> {
-}
